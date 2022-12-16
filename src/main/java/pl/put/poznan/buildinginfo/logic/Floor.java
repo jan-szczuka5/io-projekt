a3ff371bac45dbfe2e47a08278f48f84761fa3a1
Hanna Pieniążek <79666838+hannapieniazek@users.noreply.github.com>
2022-12-17 00:39:01 +0100

    Add javadoc comments

--- src/main/java/pl/put/poznan/buildinginfo/logic/Floor.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Floor.java
@@ -3,17 +3,35 @@ package pl.put.poznan.buildinginfo.logic;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class handles operations regarding the whole floor
+ *
+ * @author hannapieniazek
+ * @version 1.0
+ */
+
 public class Floor implements Location{
     private int id;
     private String name;
 
     List<Room> rooms = new ArrayList<>();
 
+     /**
+     * @param id id of the floor
+     * @param name name of the floor
+     */
+    
     public Floor(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+     /**
+     * Adds a room object to the list of rooms on the floor
+     *
+     * @param room room on the floor
+     */
+    
     public void addRoom(Room room) {
         rooms.add(room);
     }
@@ -39,6 +57,12 @@ public class Floor implements Location{
         return name;
     }
 
+     /**
+     * Calculates the total area of the floor based on the areas of all the rooms on that floor
+     *
+     * @return total area of the floor
+     */
+    
     @Override
     public double getArea() {
         double total = 0F;
@@ -48,6 +72,12 @@ public class Floor implements Location{
         return total;
     }
 
+     /**
+     * Calculates the total cubic capacity of the floor based on the cubic capacities of all the rooms on that floor
+     *
+     * @return total cubic capacity of the floor
+     */
+    
     @Override
     public double getCube() {
         double total = 0F;
@@ -57,6 +87,12 @@ public class Floor implements Location{
         return total;
     }
 
+     /**
+     * Calculates the total energy used for heating on the floor based on the energy used for heating in all the rooms on that floor
+     *
+     * @return total energy used for heating on the floor
+     */
+    
     @Override
     public double getHeating() {
         double total = 0F;
@@ -66,6 +102,12 @@ public class Floor implements Location{
         return total;
     }
 
+     /**
+     * Calculates the total lighting power of the floor based on the lighting power of all the rooms on that floor
+     *
+     * @return total lighting power of the floor
+     */
+    
     @Override
     public double getLight() {
         double total = 0F;
