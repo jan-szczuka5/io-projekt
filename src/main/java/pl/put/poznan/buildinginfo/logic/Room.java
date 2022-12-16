a72803576c523ed33753cdc2f286493d986fe470
Hanna Pieniążek <79666838+hannapieniazek@users.noreply.github.com>
2022-12-17 00:46:24 +0100

    Add javadoc comments

--- src/main/java/pl/put/poznan/buildinginfo/logic/Room.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Room.java
@@ -1,5 +1,12 @@
 package pl.put.poznan.buildinginfo.logic;
 
+/**
+ * This class handles operations regarding the room
+ *
+ * @author hannapieniazek
+ * @version 1.0
+ */
+
 public class Room implements Location{
     int id;
     String name;
@@ -8,7 +15,16 @@ public class Room implements Location{
     double cube;
     double heating;
     double light;
-
+    
+     /**
+     * @param id id of the room
+     * @param name name of the room
+     * @param area area of the room
+     * @param cube cubic capacity of the room
+     * @param heating energy used for heating in the room
+     * @param light lighting power of the room
+     */
+    
     public Room(int id, String name, double area, double cube, double heating, double light){
         this.id = id;
         this.name = name;
