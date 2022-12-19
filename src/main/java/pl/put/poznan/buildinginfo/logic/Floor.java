08c0d619fbd4dc47d73d56800b81a6539317ae5b
Hanna Pieniążek <79666838+hannapieniazek@users.noreply.github.com>
2022-12-19 15:55:59 +0100

    Update javadoc comments

--- src/main/java/pl/put/poznan/buildinginfo/logic/Floor.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Floor.java
@@ -6,6 +6,9 @@ import java.util.List;
 /**
  * This class handles operations regarding the whole floor
  *
+ * @author julia-bit
+ * @author jan-szczuka5
+ * @author Johnybonny
  * @author hannapieniazek
  * @version 1.0
  */
