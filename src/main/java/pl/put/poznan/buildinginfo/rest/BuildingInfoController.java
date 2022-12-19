36f159c2aca4cc115512cd001297ab1ac26c910d
Johnybonny <szympast@gmail.com>
2022-12-19 16:50:41 +0100

    Finish logging

--- src/main/java/pl/put/poznan/buildinginfo/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/buildinginfo/rest/BuildingInfoController.java
@@ -16,43 +16,55 @@ public class BuildingInfoController
     @PostMapping("/getArea/{id}")
     @ResponseBody
     public String GetArea(@RequestBody String json, @PathVariable String id) {
+        logger.info("Loading json file");
         BuildingInfo buildingInfo = new BuildingInfo();
         buildingInfo.loadALlBuildingsFromJson(json);
 
-        logger.debug(id);
+        logger.info("Calculating area for: " + id);
         Location location = buildingInfo.findSpecificLocationById(Integer.parseInt(id));
         if(location == null) {
+            logger.info("Wrong id");
             return "Wrong id";
         }
-        return String.valueOf(location.getArea());
+        String result = String.valueOf(location.getArea());
+        logger.info("Area calculated successfully: " + result);
+        return result;
     }
 
     @PostMapping("/getCube/{id}")
     @ResponseBody
     public String GetCube(@RequestBody String json, @PathVariable String id) {
+        logger.info("Loading json file");
         BuildingInfo buildingInfo = new BuildingInfo();
         buildingInfo.loadALlBuildingsFromJson(json);
 
-        logger.debug(id);
+        logger.info("Calculating cube for: " + id);
         Location location = buildingInfo.findSpecificLocationById(Integer.parseInt(id));
         if(location == null) {
+            logger.info("Wrong id");
             return "Wrong id";
         }
-        return String.valueOf(location.getCube());
+        String result = String.valueOf(location.getCube());
+        logger.info("Cube calculated successfully: " + result);
+        return result;
     }
 
     @PostMapping("/getLightPerArea/{id}")
     @ResponseBody
     public String GetLightPerArea(@RequestBody String json, @PathVariable String id) {
+        logger.info("Loading json file");
         BuildingInfo buildingInfo = new BuildingInfo();
         buildingInfo.loadALlBuildingsFromJson(json);
 
-        logger.debug(id);
+        logger.info("Calculating light per area for: " + id);
         Location location = buildingInfo.findSpecificLocationById(Integer.parseInt(id));
         if(location == null) {
+            logger.info("Wrong id");
             return "Wrong id";
         }
-        return String.valueOf(location.getLight()/location.getArea());
+        String result = String.valueOf(location.getLight()/location.getArea());
+        logger.info("Light per area calculated successfully: " + result);
+        return result;
     }
 
 }
