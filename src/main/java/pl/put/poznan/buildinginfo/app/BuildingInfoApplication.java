223b55befe293a3f9a639a01406fd0b8a8a1f316
Hanna Pieniążek <79666838+hannapieniazek@users.noreply.github.com>
2023-01-24 01:26:50 +0100

    Add class documentation

--- src/main/java/pl/put/poznan/buildinginfo/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildinginfo/app/BuildingInfoApplication.java
@@ -4,8 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.buildinginfo.rest"})
+
+/**
+ * This class is responsible for running the application
+ *
+ * @author julia-bit
+ * @author jan-szczuka5
+ * @author Johnybonny
+ * @author hannapieniazek
+ * @version 1.0
+ */
+
 public class BuildingInfoApplication
 {
+    
+     /**
+     * This is the main function, which runs the application
+     *
+     * @param args
+     */
+    
     public static void main (String[] args)
     {
         SpringApplication.run(BuildingInfoApplication.class, args);
